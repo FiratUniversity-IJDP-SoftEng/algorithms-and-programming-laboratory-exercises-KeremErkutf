48cc42540296ba8b7674f3f4a51f640fb895b45c
kerem <kciftlikci@gmail.com>
2025-03-10 17:01:22 +0300

    ex004.py is completed.

--- Exercises/ex004.py
+++ Exercises/ex004.py
@@ -1,2 +1,12 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 240543013
+# Your Name and Surname: Kerem Erkut Çiftlikçi
+
+def calculate_area_of_circle(radius):
+    return 3.14 * radius ** 2
+
+r = float(input("Enter the radius of the circle: "))
+area = calculate_area_of_circle(r)
+print(f"Area of the circle: {area:.2f}")
+
+
+
