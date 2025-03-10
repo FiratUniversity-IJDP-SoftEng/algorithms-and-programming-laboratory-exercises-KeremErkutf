19fb241d4ce02a9535597e9d5bca2cd6dae11d7f
kerem <kciftlikci@gmail.com>
2025-03-10 17:19:34 +0300

    ex001.py is completed.

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 240543013
+# Your Name and Surname: Kerem Erkut Çiftlikçi
+
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are!") 
+print("\t\tUp above the world so high,")   		
+print("\t\tLike a diamond in the sky.") 
+print("Twinkle, twinkle, little star,") 
+print("\tHow I wonder what you are")
